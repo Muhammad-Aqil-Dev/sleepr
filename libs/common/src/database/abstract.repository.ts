33df907a830fffc05fdfec3027b5a8a90e09c77b
Aqil7845 <k247845@nu.edu.com>
2025-07-21 06:33:57 +0500

    Reservation CRUD

--- libs/common/src/database/abstract.repository.ts
+++ libs/common/src/database/abstract.repository.ts
@@ -16,7 +16,7 @@ export abstract class AbstractRepository<TDocument extends AbstractDocument> {
   }
 
   async findOne(filterQuery: FilterQuery<TDocument>): Promise<TDocument> {
-    const document = await this.model.findOne(filterQuery, {}, { lean: true });
+    const document = await this.model.findOne(filterQuery, {}, { lean: false });
 
     if (!document) {
       this.logger.warn('Document not found with filterQuery', filterQuery);
