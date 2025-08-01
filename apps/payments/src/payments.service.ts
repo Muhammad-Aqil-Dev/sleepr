2803f37cd8a6df1ffecef78eebe5a7f43976c2bf
Aqil <muhammad.aqil.dev@gmail.com>
2025-08-01 14:24:49 +0500

    Stripe setup

---
+++ apps/payments/src/payments.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import Stripe from 'stripe';
+import { CreateChargeDto } from './dto/create-charge.dto';
+
+@Injectable()
+export class PaymentsService {
+  private readonly stripe;
+  constructor(private readonly configService: ConfigService) {
+    this.stripe = new Stripe(
+      this.configService.get<string>("STRIPE_SECRET_KEY")!,
+      {
+        apiVersion: '2025-07-30.basil'
+      }
+
+    );
+
+  }
+
+  async createCharge({ card, amount }: CreateChargeDto) {
+    const paymentMethod = await this.stripe.paymentMethods.create({
+      type: 'card',
+      card,
+    });
+
+    const paymentIntent = await this.stripe.paymentIntents.create({
+      payment_method: paymentMethod.id,
+      amount: amount * 100,
+      payment_method_types: ['cards'],
+      currency: 'usd'
+    })
+
+    return paymentIntent;
+  }
+}
