5433e54d0cf278be4930d91083b3591ae3ad0ebd
Aqil <muhammad.aqil.dev@gmail.com>
2025-10-02 22:15:26 +0500

    health check

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from './users/users.module';
-import { LoggerModule } from 'default/common';
+import { HealthModule, LoggerModule } from 'default/common';
 import { JwtModule } from '@nestjs/jwt'
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import * as  Joi from 'joi';
@@ -31,7 +31,9 @@ import { JwtStrategy } from './strategies/jwt.strategy';
           expiresIn: `${configService.get('JWT_Expiration')}s`
         }
       })
-    })],
+    }),
+    HealthModule
+  ],
   controllers: [AuthController],
   providers: [AuthService, LocalStrategy, JwtStrategy]
 })
